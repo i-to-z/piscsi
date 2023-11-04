6fce1e7df1269dec2e657349d5632adb484a0b49
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-11-04 18:31:40 +0100

    Update logging

--- cpp/hal/in_process_bus.cpp
+++ cpp/hal/in_process_bus.cpp
@@ -50,8 +50,6 @@ pair<bool, string> InProcessBus::FindSignal(int pin) const
 {
 	const auto& it = signals.find(pin);
 	if (it == signals.end()) {
-		spdlog::critical("Unhandled signal pin " + to_string(pin));
-		assert(false);
 		return { false, "????" };
 	}
 
@@ -69,14 +67,14 @@ bool DelegatingInProcessBus::Init(mode_e mode)
 
 bool DelegatingInProcessBus::GetSignal(int pin) const
 {
-	const auto& [state, _] = bus.FindSignal(pin);
+	const auto [state, _] = bus.FindSignal(pin);
 
 	return bus.GetSignal(pin);
 }
 
 void DelegatingInProcessBus::SetSignal(int pin, bool state)
 {
-	const auto& [_, signal] = bus.FindSignal(pin);
+	const auto [_, signal] = bus.FindSignal(pin);
 
 	spdlog::trace(GetMode() + ": Setting " + signal + " to " + (state ? "true" : "false"));
 
