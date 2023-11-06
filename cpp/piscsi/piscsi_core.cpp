aca845058f84e031d67a41910881421feb70c964
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-11-06 21:15:04 +0100

    Update TODO

--- cpp/piscsi/piscsi_core.cpp
+++ cpp/piscsi/piscsi_core.cpp
@@ -80,7 +80,7 @@ void Piscsi::CleanUp()
 {
 	service.Stop();
 
-	// TODO When devices are attached this sometimes crashes on a regular PC due to an unknown race condition
+	// TODO When devices are attached this sometimes crashes on a regular PC on Ctrl-C
 	executor->DetachAll();
 
 	bus->Cleanup();
