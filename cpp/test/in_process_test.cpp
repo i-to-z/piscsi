3cfb4f64d3678a25a26bb28749ea59a4d216c08f
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-11-09 11:18:53 +0100

    Update formatting

--- cpp/test/in_process_test.cpp
+++ cpp/test/in_process_test.cpp
@@ -15,56 +15,56 @@
 using namespace std;
 using namespace piscsi_util;
 
-void add_arg(vector<char *>& args, const string& arg)
+void add_arg(vector<char*> &args, const string &arg)
 {
-	args.push_back(strdup(arg.c_str()));
+    args.push_back(strdup(arg.c_str()));
 }
 
 int main(int argc, char *argv[])
 {
-	string t_args;
-	string i_args;
+    string t_args;
+    string i_args;
 
-	int opt;
-	while ((opt = getopt(argc, argv, "-i:t:")) != -1) {
-		switch (opt) {
-			case 'i':
-				i_args = optarg;
-				break;
+    int opt;
+    while ((opt = getopt(argc, argv, "-i:t:")) != -1) {
+        switch (opt) {
+        case 'i':
+            i_args = optarg;
+            break;
 
-			case 't':
-				t_args = optarg;
-				break;
+        case 't':
+            t_args = optarg;
+            break;
 
-			default:
-				cerr << "Parser error" << endl;
-				exit(EXIT_FAILURE);
-				break;
-		}
-	}
+        default:
+            cerr << "Parser error" << endl;
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
 
-	vector<char *> initiator_args;
-	add_arg(initiator_args, "initiator");
-	for (const auto& arg : Split(i_args, ' ')) {
-		add_arg(initiator_args, arg);
-	}
+    vector<char*> initiator_args;
+    add_arg(initiator_args, "initiator");
+    for (const auto &arg : Split(i_args, ' ')) {
+        add_arg(initiator_args, arg);
+    }
 
-	vector<char *> target_args;
-	add_arg(target_args, "target");
-	for (const auto& arg : Split(t_args, ' ')) {
-		add_arg(target_args, arg);
-	}
+    vector<char*> target_args;
+    add_arg(target_args, "target");
+    for (const auto &arg : Split(t_args, ' ')) {
+        add_arg(target_args, arg);
+    }
 
-	auto target_thread = jthread([&target_args] () {
-		auto piscsi = make_unique<Piscsi>();
-		piscsi->run(target_args, true);
-	});
+    auto target_thread = jthread([&target_args]() {
+        auto piscsi = make_unique<Piscsi>();
+        piscsi->run(target_args, true);
+    });
 
-	// TODO Avoid sleep
-	sleep(1);
+    // TODO Avoid sleep
+    sleep(1);
 
-	auto scsidump = make_unique<ScsiDump>();
-	scsidump->run(initiator_args, true);
+    auto scsidump = make_unique<ScsiDump>();
+    scsidump->run(initiator_args, true);
 
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
