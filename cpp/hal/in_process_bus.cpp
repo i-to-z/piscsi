00bd0f2575e0ac04497601fad994f1de74eae78e
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-11-09 10:59:12 +0100

    Update formatting

--- cpp/hal/in_process_bus.cpp
+++ cpp/hal/in_process_bus.cpp
@@ -12,25 +12,25 @@
 
 void InProcessBus::Reset()
 {
-	signals = {};
+    signals = { };
 
-	dat = 0;
+    dat = 0;
 }
 
 bool InProcessBus::GetSignal(int pin) const
 {
-	return signals[pin];
+    return signals[pin];
 }
 
 void InProcessBus::SetSignal(int pin, bool state)
 {
-	scoped_lock lock(write_locker);
-	signals[pin] = state;
+    scoped_lock lock(write_locker);
+    signals[pin] = state;
 }
 
 bool InProcessBus::WaitSignal(int pin, bool state)
 {
-	const auto now = chrono::steady_clock::now();
+    const auto now = chrono::steady_clock::now();
 
     do {
         if (signals[pin] == state) {
@@ -38,7 +38,7 @@ bool InProcessBus::WaitSignal(int pin, bool state)
         }
 
         if (signals[PIN_RST]) {
-        	return false;
+            return false;
         }
     } while ((chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - now).count()) < 3);
 
@@ -47,51 +47,51 @@ bool InProcessBus::WaitSignal(int pin, bool state)
 
 bool InProcessBus::WaitForSelectEvent()
 {
-	// TODO Is there a better way to avoid busy waiting?
-	const timespec ts = { .tv_sec = 0, .tv_nsec = 1'000'000 };
-	nanosleep(&ts, nullptr);
+    // TODO Is there a better way to avoid busy waiting?
+    const timespec ts = { .tv_sec = 0, .tv_nsec = 1'000'000 };
+    nanosleep(&ts, nullptr);
 
-	return true;
+    return true;
 }
 
 void DelegatingInProcessBus::Reset()
 {
-	spdlog::trace(GetMode() + ": Resetting bus");
+    spdlog::trace(GetMode() + ": Resetting bus");
 
-	bus.Reset();
+    bus.Reset();
 }
 
 bool DelegatingInProcessBus::GetSignal(int pin) const
 {
-	const bool state = bus.GetSignal(pin);
+    const bool state = bus.GetSignal(pin);
 
-	if (log_signals && pin != PIN_ACK && pin != PIN_REQ && spdlog::get_level() == spdlog::level::trace) {
-		spdlog::trace(GetMode() + ": Getting " + GetSignalName(pin) + (state ? ": true" : ": false"));
-	}
+    if (log_signals && pin != PIN_ACK && pin != PIN_REQ && spdlog::get_level() == spdlog::level::trace) {
+        spdlog::trace(GetMode() + ": Getting " + GetSignalName(pin) + (state ? ": true" : ": false"));
+    }
 
-	return state;
+    return state;
 }
 
 void DelegatingInProcessBus::SetSignal(int pin, bool state)
 {
-	if (log_signals && pin != PIN_ACK && pin != PIN_REQ && spdlog::get_level() == spdlog::level::trace) {
-		spdlog::trace(GetMode() + ": Setting " + GetSignalName(pin) + " to " + (state ? "true" : "false"));
-	}
+    if (log_signals && pin != PIN_ACK && pin != PIN_REQ && spdlog::get_level() == spdlog::level::trace) {
+        spdlog::trace(GetMode() + ": Setting " + GetSignalName(pin) + " to " + (state ? "true" : "false"));
+    }
 
-	bus.SetSignal(pin, state);
+    bus.SetSignal(pin, state);
 }
 
 bool DelegatingInProcessBus::WaitSignal(int pin, bool state)
 {
-	if (log_signals && pin != PIN_ACK && pin != PIN_REQ && spdlog::get_level() == spdlog::level::trace) {
-		spdlog::trace(GetMode() + ": Waiting for " + GetSignalName(pin) + " to become " + (state ? "true" : "false"));
-	}
+    if (log_signals && pin != PIN_ACK && pin != PIN_REQ && spdlog::get_level() == spdlog::level::trace) {
+        spdlog::trace(GetMode() + ": Waiting for " + GetSignalName(pin) + " to become " + (state ? "true" : "false"));
+    }
 
-	return bus.WaitSignal(pin, state);
+    return bus.WaitSignal(pin, state);
 }
 
 string DelegatingInProcessBus::GetSignalName(int pin) const
 {
-	const auto& it = SIGNALS.find(pin);
-	return it != SIGNALS.end() ? it->second : "????";
+    const auto &it = SIGNALS.find(pin);
+    return it != SIGNALS.end() ? it->second : "????";
 }
