c25d51e419240db69075fdf70e86c165bc55c63c
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-11-14 19:05:02 +0100

    Update logging

--- cpp/devices/host_services.cpp
+++ cpp/devices/host_services.cpp
@@ -184,7 +184,7 @@ bool HostServices::ExecuteCommand(const CommandContext& context, PbResult& resul
         return context.ReturnLocalizedError(LocalizationKey::ERROR_OPERATION, UNKNOWN_OPERATION, to_string(operation));
     }
 
-    spdlog::trace("Received " + PbOperation_Name(operation) + " command");
+    LogTrace("Received " + PbOperation_Name(operation) + " command");
 
     switch (operation) {
     case LOG_LEVEL:
